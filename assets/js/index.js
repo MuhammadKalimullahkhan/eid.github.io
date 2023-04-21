43b5a179155ff72b4c5c87cd8529270525fd4a4b
MuhammadKalimullahkhan <khankalimkalim121@gmail.com>
2023-04-21 13:08:30 +0500

    v1.0.3

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,6 @@
 function domContentLoaded(e) {
-
-    const text1 = document.getElementById("text1");
-    const text2 = document.getElementById("text2");
-
-    text1.addEventListener("animationend", () => {
-        text2.style.animationDelay = "0s";
-    });
-
-    // let name = prompt("What is your name?");
-    // document.querySelector("name").textContent = name;
+    let name = prompt("What is your name?");
+    document.querySelector("name").textContent = name;
     document.querySelectorAll("year")?.forEach((e) => {
         e.textContent = new Date().getFullYear();
     });
